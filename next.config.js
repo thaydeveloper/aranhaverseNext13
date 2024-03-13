c6ede5a4ff29a7f04c043925aefda35101d826ea
thaydeveloper <thaydeveloper26@gmail.com>
2024-03-13 19:27:53 -0300

    feito a instalacoes de alguns pacotes, e o header da pagina, e adicionadoa algumas estilizasoes

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    API_URL: "https://mocki.io/v1/3ebe16c1-6e7d-492e-b736-655739987131",
+  },
+};
+
+module.exports = nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/*  @type {import('next').NextConfig} 
+const nextConfig = {
+  env: {
+    API_URL: "https://mocki.io/v1/92910fd6-6f6c-40e6-980e-135311434d43",
+  },
+};
 
 export default nextConfig;
+ */
